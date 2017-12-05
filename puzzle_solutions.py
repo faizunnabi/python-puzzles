44b0da8d937f5471402cce36eb4027945e36aeea
Syed Faizun Nabi <faizunnabi@users.noreply.github.com>
2017-12-05 15:50:05 +0400

    Create puzzle_solutions.py

---
+++ puzzle_solutions.py
@@ -0,0 +1,38 @@
+#function to check most wanted character
+def checkio(text):
+    tc=text.lower()
+    ln=len(tc)
+    count={}
+    result={}
+    al=''
+    for c in tc:
+        if c.isalpha():
+            if c in count:
+                count[c]+=1
+            else:
+                count[c]=1
+    #al= max(count, key=count.get)
+    for val in count:
+        if count[val] in result:
+            result[count[val]].append(val)
+        else:
+            result[count[val]] = [val]
+    max_key=max(result)
+    if len(result[max_key])>1:
+        al=min(result[max_key])
+    else:
+        al=result[max_key][0]
+        
+    return al
+
+#function to check repeated elements in list
+def checkio(data):
+    an_list=[]
+    dup_list=[]
+    for d in data:
+        if d in an_list:
+            dup_list.append(d)
+        else:
+            an_list.append(d)
+    lt=[x for x in data if x in dup_list]
+    return lt
