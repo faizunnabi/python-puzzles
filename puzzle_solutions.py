b935e29a8ba27ed67c6cd03b0b34455e98271c9f
Syed Faizun Nabi <faizunnabi@users.noreply.github.com>
2017-12-05 16:18:48 +0400

    Update puzzle_solutions.py

--- puzzle_solutions.py
+++ puzzle_solutions.py
@@ -36,3 +36,14 @@ def checkio(data):
             an_list.append(d)
     lt=[x for x in data if x in dup_list]
     return lt
+
+#function for password strength
+def checkio(data):
+    import re
+    pass_len=len(data)
+    if pass_len>=10:
+        if any(x.isupper() for x in data) and any(x.islower() for x in data) and any(x.isdigit() for x in data):   
+            print(data)
+            return True
+        #replace this for solution
+    return False
